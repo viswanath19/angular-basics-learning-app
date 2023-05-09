9389d364d78d3402a45582330e46f7a107afcbae
Viswanath <allamrajuviswanath@gmail.com>
2023-05-10 00:29:34 +0530

    class bindings

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -2,10 +2,34 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-test',
-  templateUrl: './test.component.html',
-  styleUrls: ['./test.component.css']
+  template: `
+    <h2 class="text-success">CompView</h2>
+    <h2 class="text-danger">CompView</h2>
+    <h2 class="text-special">CompView</h2>
+    <h2 [class]="textSuccess">CompView</h2>
+    <h2 [class.text-danger]="hasError">CompView Error</h2>
+    <h2 [ngClass]="messageClass">CompView Message Classes</h2>
+  `,
+  styles: [`
+    .text-success {
+      color: green;
+    }
+    .text-danger {
+      color: red;
+    }
+    .text-special {
+      font-style: italic;
+    }
+  `]
 })
 export class TestComponent {
+
+  textSuccess: string = "text-success";
+  hasError: boolean = true;
+  messageClass: object = {
+    "text-success": !this.hasError,
+    "text-danger": this.hasError
+  }
 
   name: string = "TestWorks";
 
